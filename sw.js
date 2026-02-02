92aec5f1e9935a640efb6e7031262e76ab1c6b2b
Boxkidd <djglasgow7@gmail.com>
2026-02-02 14:43:44 -0500

    1

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
 const CACHE_NAME = 'void-dice-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const assets = ['./', './index.html'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
